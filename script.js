4037e2a62e140daab79959eea7b005cabb048b6d
Chrissser <112469487+Chrissser@users.noreply.github.com>
2023-03-06 01:43:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
 const vidContainer = document.querySelector(".video-player");
-const vid = document.querySelector(".video-player video");
-const btn = document.querySelector(".video-player-button");
-
+const vid = document.getElementById("video");
+const videoControls = document.getElementById("video-controls");
+const videoWorks = !!document.getElementById("video").canPlayType;
+if (videoworks) {
+  video.controls = false;
+  videoControls.classList.remove("hidden");
+}
+const playbutton = document.getElementById("play");
+const playbackAnimation = document.getElementById("playback-animation");
 function togglePlay() {
-  click;
+  if (video.paused || video.Ended) {
+    video.play();
+  } else {
+    video.paused();
+  }
 }
 
 function videoEnded() {
   ended;
 }
-
+// animatePlayback displays an animation when
+// the video is played or paused
+function animatePlayback() {
+  playbackAnimation.animate(
+    [
+      {
+        opacity: 1,
+        transform: "scale(1)",
+      },
+      {
+        opacity: 0,
+        transform: "scale(1.3)",
+      },
+    ],
+    {
+      duration: 500,
+    }
+  );
+}
 btn.addEventListener("click", togglePlay);
 vid.addEventListener("ended", videoEnded);
+video.addEventListener("click", animatePlayback);
